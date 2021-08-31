7ab4d06fa33dd1360990e9355f4adfde58efb95d
Yuki Wang <moonyw0730@gmail.com>
2021-08-31 22:16:57 +1000

    Add solutions

--- TheHurdleRace.js
+++ TheHurdleRace.js
@@ -1,4 +1,5 @@
 //https://www.hackerrank.com/challenges/the-hurdle-race/problem
+//FirstSolution
 function hurdleRace(k, height) {
     let dose = 0;
     let highest = Math.max(...height);
@@ -7,5 +8,22 @@ function hurdleRace(k, height) {
     } else {
         dose = 0;
     }
+    return dose
+}
+
+//SecondSolution
+function hurdleRace(k, height) {
+    let dose = 0;
+    let highest = 0;
+    for (let i = 0; i < height.length; i++){
+        if (height[i] > highest){
+        highest = height[i];
+        }
+    }
+        if (k < highest){
+            dose = highest - k;
+        } else {
+            dose = 0;
+        }
     return dose
 }

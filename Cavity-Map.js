08af7d1b43057e578813546c43128816f7043d33
Yuki Wang <moonyw0730@gmail.com>
2021-10-19 22:56:19 +1100

    Add cavity map

--- Cavity-Map.js
+++ Cavity-Map.js
@@ -1,1 +1,21 @@
 //https://www.hackerrank.com/challenges/cavity-map/problem
+function cavityMap(grid) {
+    let res = [];
+    let newArray = [];
+    for (let k = 0; k < grid.length; k++){
+        newArray.push(grid[k])
+    }
+    
+    for(let i = 1; i < grid.length - 1; i++){
+        for(let j = 1; j < grid[0].length - 1; j++){
+            if (newArray[i][j] > newArray[i - 1][j] && 
+            newArray[i][j] > newArray[i + 1][j] && 
+            newArray[i][j] > newArray[i][j - 1] && 
+            newArray[i][j] > newArray[i][j + 1]){
+                grid[i] = grid[i].slice(0, j) + "X" + grid[i].slice(j + 1);
+            }
+        }
+    }
+    
+    return grid;
+}
